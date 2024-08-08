7f52405fb8641ce8e8ac60c14ae65f86684d1263
sumtuckyi <sumin510@naver.com>
2024-08-08 19:49:17 +0900

    8월 8일

--- modularNdevideConquer_softeer.py
+++ modularNdevideConquer_softeer.py
@@ -1,16 +1,19 @@
 import sys
 
-def power(base, exponent, mod):
+def power(base, exponent, mod): # base는 밑, exponent는 지수
     result = 1
-    base = base % mod
+    base = base % mod # base가 mod보다 작거나 같게 만든다. 이 문제에서는 base < mod
     while exponent > 0:
-        if exponent % 2 == 1:
-            result = (result * base) % mod
-        exponent = exponent // 2
-        base = (base * base) % mod
+        if exponent % 2 == 1: # 지수가 홀수이면
+            result = (result * base) % mod # base를 한 번 더 곱해준다.
+        exponent = exponent // 2 # 지수를 2로 나누어 크기를 줄인다.
+        base = (base * base) % mod # base를 제곱한다. 이때 수가 커지지 않고 mod보다 작도록 연산한다.
     return result
 
 # 입력 받기
+# 1 ≤ K ≤ 10**8 인 정수
+# 1 ≤ P ≤ 10**8 인 정수
+# 1 ≤ N ≤ 10**16 인 정수
 k, p, n = map(int, sys.stdin.readline().split())
 
 # 계산
@@ -21,7 +24,8 @@ result = (k * power(p, 10 * n, mod)) % mod
 print(result)
 
 
-# 백준 1629 - 분할정복, 모듈러 연산 이용
+# 백준 1629 - 분할정복, 모듈러 연산 이용, 반복문 대신 재귀로 구현
+# A가 밑, B가 지수, C가 나누는 값
 A, B, C = map(int, input().split())
 
 

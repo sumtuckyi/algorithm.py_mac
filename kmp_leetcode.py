580d6cdd93dd51656c36925aa366e0f875f5b303
sumtuckyi <sumin510@naver.com>
2024-09-20 23:50:16 +0900

    9월 20일

--- kmp_leetcode.py
+++ kmp_leetcode.py
@@ -5,20 +5,20 @@ class Solution:
         new_s = s + '#' + reversed_s
 
         def build_lps(pattern):
-            lps = [0] * len(pattern)
-            length = 0  # length of the previous longest prefix suffix
-            i = 1
+            lps = [0] * len(pattern) # pattern[:i]에서 접두사-접미사의 최장 길이를 저장
+            length = 0  # 이전까지 일치한 접두사-접미사의 길이
+            i = 1 # lps[0]은 항상 0이므로 1부터 탐색 시작
 
             while i < len(pattern):
-                if pattern[i] == pattern[length]:
+                if pattern[i] == pattern[length]: # 일치한다면 접두사-접미사의 길이를 늘린다.
                     length += 1
                     lps[i] = length
                     i += 1
-                else:
+                else: # 일치하지 않을 때,
                     if length != 0:
-                        length = lps[length - 1]
-                    else:
-                        lps[i] = 0
+                        length = lps[length - 1] # 이전에 일치한 접두사-접미사 길이의 위치로 돌아가서 비교, i는 그대로
+                    else: # 패턴의 맨 앞 문자와 비교 중이었다면
+                        lps[i] = 0 # 접두사-접미사는 0인 것임
                         i += 1
             return lps
 
